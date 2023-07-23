abd4ffc26afe9b6a9224c2e58692fee0d3fbd02c
haohaoguo-666 <yyf1519224631@gmail.com>
2023-07-23 23:59:40 +0800

    list模拟

---
+++ 初阶/list/list模拟/Test.cpp
@@ -0,0 +1,44 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <iostream>
+#include <list>
+using namespace std;
+
+void test_list1()
+{
+	list<int> lt;
+	lt.push_back(1);
+	lt.push_back(2);
+	lt.push_back(3);
+	lt.push_back(4);
+
+	list<int>::iterator it = lt.begin();
+	while (it != lt.end())
+	{
+		cout << *it << " ";
+		++it;
+	}
+
+	cout << endl;
+
+	
+	list<int>::iterator pos = find(lt.begin(), lt.end(), 3);
+	if (pos != lt.end())
+	{
+		lt.insert(pos, 30);
+	}
+
+	for (auto e : lt)
+	{
+		cout << e << " ";
+	}
+
+	cout << endl;
+	
+}
+
+
+//int main()
+//{
+//	test_list1();
+//	return 0;
+//}
